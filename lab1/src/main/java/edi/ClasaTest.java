ff350c508238d2a01703d78157a95a5988e46324
EdiBOI25 <jaguarul2501@gmail.com>
2025-02-25 08:53:33 +0200

    al doilea commit

--- lab1/src/main/java/edi/ClasaTest.java
+++ lab1/src/main/java/edi/ClasaTest.java
@@ -3,5 +3,6 @@ package edi;
 public class ClasaTest {
     public static void main(String[] args) {
         System.out.printf("Testam git");
+        System.out.println("testam din nou");
     }
 }
