ae32fddfff61d06a36194d5a079a0d5733609f72
Eduard Cioropariu <jaguarul2501@gmail.com>
2025-03-25 09:33:12 +0200

    added lab4

---
+++ lab4/src/main/java/edi/repository/ComputerRepairedFormRepository.java
@@ -0,0 +1,10 @@
+package edi.repository;
+
+import edi.model.ComputerRepairedForm;
+
+import java.util.List;
+
+public interface ComputerRepairedFormRepository extends Repository<Integer, ComputerRepairedForm> {
+    List<ComputerRepairedForm> filterByEmployee(String employee);
+    List<ComputerRepairedForm> filterByEmployeeAndDate(String employee, String date);
+}
